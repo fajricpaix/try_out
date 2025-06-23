ca9544c02ec22f3476fd666d4010c85dfe74123b
omstephen <omstephen@lidowin.com>
2025-06-23 12:01:48 +0700

    add ads on main view

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:try_out/views/home/content/menu.dart';
 import 'package:try_out/views/home/header/header.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart'; // Import library AdMob
 
 void main() {
+  // binding Flutter initial before AdMob
+  WidgetsFlutterBinding.ensureInitialized();
+  // Inisialisasi Google Mobile Ads SDK
+  MobileAds.instance.initialize();
   runApp(const MyApp());
 }
 
@@ -13,16 +18,71 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'CPNS Try Out 2025',
-      theme: ThemeData(
-      colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late BannerAd _bannerAd;
+  bool _isBannerAdLoaded = false;
+
+  // Android Test ID: ca-app-pub-3940256099942544/6300978111
+  final String bannerAdUnitId = 'ca-app-pub-3940256099942544/6300978111';
+  // Production ID
+  // final String bannerAdUnitId = 'ca-app-pub-2602479093941928/9052001071';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadBannerAd();
+  }
+
+  void _loadBannerAd() {
+    _bannerAd = BannerAd(
+      adUnitId: bannerAdUnitId,
+      size: AdSize.banner, // standar size for ads
+      request: const AdRequest(),
+      listener: BannerAdListener(
+        onAdLoaded: (Ad ad) {
+          // Callback ads if done to show
+          setState(() {
+            _isBannerAdLoaded = true;
+          });
+        },
+        onAdFailedToLoad: (Ad ad, LoadAdError error) {
+          // Callback ads if no show
+          ad.dispose(); // Delet ads for lost perfomance
+          setState(() {
+            _isBannerAdLoaded = false; // Set to false for not showing
+          });
+        },
       ),
-      home: Scaffold(
+    )..load(); // Start ads process
+  }
+
+  @override
+  void dispose() {
+    // Important: Remove ads when widget not use for free perfomance
+    _bannerAd.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       backgroundColor: const Color(0xFF5E00B0),
       body: SingleChildScrollView(
         padding: const EdgeInsets.only(top: 32.0),
-        child: 
-          Column(
-            children: [
+        child: Column(
+          children: [
             SizedBox(
               height: MediaQuery.of(context).size.height,
               child: Column(
@@ -35,7 +95,16 @@ class MyApp extends StatelessWidget {
           ],
         ),
       ),
-    ),
+      // Add bottomNavigationBar for adsBanner
+      bottomNavigationBar: _isBannerAdLoaded
+          ? Container(
+              color: Colors.white,
+              width: _bannerAd.size.width.toDouble(),
+              height: _bannerAd.size.height.toDouble(),
+              alignment: Alignment.center,
+              child: AdWidget(ad: _bannerAd), // Widget to show ads
+            )
+          : const SizedBox.shrink(), // If ads not show, show empty widget
     );
   }
 }
