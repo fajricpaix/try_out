c97031d5f65c53d0e3b9091372a13e84b7225d9d
omstephen <omstephen@lidowin.com>
2025-08-27 11:53:41 +0700

    delete pdf

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
     }
 
     defaultConfig {
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
